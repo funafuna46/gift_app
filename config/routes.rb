6811fd2c7380ed0e5f3b0d4467924d1231ee3d69
funafuna46 <orbystana@yahoo.co.jp>
2024-02-12 19:05:34 +0900

    add: ギフト券送信完了ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get 'static_pages/completion'
+
   resources :template_gifts, only: %i[index] do
     collection do
       post :preview
     end
   end
+
   resources :original_gifts, only: %i[index] do
     collection do
       post :preview

6a20863d6ee87bbb24c88df9c2e74e8e868dfefa
funafuna46 <orbystana@yahoo.co.jp>
2024-02-14 15:23:39 +0900

    add: バックエンドでのログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   get 'static_pages/completion'
   get 'static_pages/select_gift_type'
 
+  resources :users, only: %i[show create]
+  resources :gifts, only: %i[index show]
+
   resources :template_gifts, only: %i[index] do
     collection do
       post :preview
     end
   end
 
-  resources :original_gifts, only: %i[index] do
+  resources :original_gifts, only: %i[index create] do
     collection do
       post :preview
     end

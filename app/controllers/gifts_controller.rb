bcb895950c639fc5692df3b4276d25f7d70ef252
funafuna46 <orbystana@yahoo.co.jp>
2024-02-20 10:52:47 +0900

    update: ギフト券一覧表示方法の調整

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,6 +2,7 @@ class GiftsController < ApplicationController
   def index
     @gifts = Gift.all
     @template_gifts = GiftCardTemplate.where(has_message: true)
+    @all_gifts = @gifts + @template_gifts
   end
 
   def show
